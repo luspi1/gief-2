7bc2e99773a30d4a88c184401c2871fe0f26e5f9
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-05-31 17:39:08 +0300

    v.0.0.1: 5 страниц(стационарки)

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,5 +1,19 @@
-import {Swiper} from "swiper/bundle";
+import { Swiper } from "swiper/bundle"
 
 
 // Инициализация слайдеров
 
+export const swiperHeader = new Swiper('.news-item-page__slider', {
+  navigation: {
+    nextEl: '.news-item-page__slider-button-next',
+    prevEl: '.news-item-page__slider-button-prev'
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction'
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 5,
+})
